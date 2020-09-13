35a2070cec0a048df788f311ab078e824899d033
philastrio <sideproject0214@gmail.com>
2020-09-13 19:24:45 +0900

    (2) Echo Server_query

--- main.go
+++ main.go
@@ -11,11 +11,33 @@ func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Success")
 }
 
+func getCats(c echo.Context) error {
+	catName := c.QueryParam("name")
+	catType := c.QueryParam("type")
+	dataType := c.Param("data")
+
+	if dataType == "string" {
+		return c.String(http.StatusOK, fmt.Sprintf("your cat name is: %s\nand his type is:%s\n", catName, catType))
+	}
+
+	if dataType == "json" {
+		return c.JSON(http.StatusOK, map[string]string{
+			"name": catName,
+			"type": catType,
+		})
+	}
+
+	return c.JSON(http.StatusBadRequest, map[string]string{
+		"error": "you need to lets us know if you want json or string data",
+	})
+}
+
 func main() {
 	fmt.Println("Hello")
 
 	e := echo.New()
 	e.GET("/", hello)
+	e.GET("/cats/:data", getCats)
 
 	e.Start(":7000")
 }
